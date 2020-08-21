c709b46cd3db13b9c68b44b4e2ca50748d070050
Gustavo Couto <gustavo.couto@trentim.com>
2020-08-21 00:54:07 -0300

    flask procfile

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def add_label():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host=0.0.0.0, port=port)
+    app.run(host='0.0.0.0', port=port)
 
 # file_path = __file__.replace('app.py', 'skew.jpg')
 # image = cv2.imread(file_path)
